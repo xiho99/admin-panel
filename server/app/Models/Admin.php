d4296047caa96ca31dca86175f92a277de3f119b
Viccent <rbtmol340@gmail.com>
2023-11-16 18:25:16 +0700

    remove not use

--- server/app/Models/Admin.php
+++ server/app/Models/Admin.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Foundation\Auth\User as Authenticatable;
